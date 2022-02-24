4557b31f577b850d47373c23d016ff4573697b3e
Matt Vaughan <admin@thenshow.me>
2022-02-24 15:16:40 -0500

    state middle

--- BlogFile.scala
+++ BlogFile.scala
@@ -2,6 +2,7 @@ package me.thenshow
 
 import java.io.File
 import scala.io.Source
+import sys.process._
 
 abstract class BlogFile {
     def render(): String
@@ -10,19 +11,19 @@ abstract class BlogFile {
 case class Pdf(f:File) extends BlogFile {
     def render(): String = {
         "<div style=\"text-align:center\"><h4>Pdf viewer testing</h4><iframe src=\"" +
-        s"https://docs.google.com/viewer?url=${f.getPath()}&embedded=true\" frameborder=\"0\" height=\"500px\" width=\"100%\"></iframe></div>"
+        "https://docs.google.com/viewer?url=" + 
+        f.getPath() + 
+        "&embedded=true\" frameborder=\"0\" height=\"500px\" width=\"100%\"></iframe></div>"
     }
 }
 
 case class Tex(f:File) extends  BlogFile {
     def render(): String = {
-        if ( !("mkdir ./tmp" !) && 
-             !("echo -e \"n\" | pdflatex -output-directory=./tmp -output-format=pdf " + f.getPath !)
-         ) {
-            val fileName = f.getName().split(".tex")[0] + ".pdf"
-            val mv_ret = "mv ./tmp/" + fileName + " ./" + fileName !
-            val rm_ret = "rm -rf ./tmp" !
-
+        if ( ("pdflatex -output-directory=./generatedPdfs -output-format=pdf " + f.getPath() !) == 0 )
+        {
+            val fileName = "./generatedPdfs" + f.getName().split(".tex").head + ".pdf"
+            println(fileName)
+ 
             val pdfFile = new File("./"+fileName)
             assert( mv_ret == 0 && pdfFile.exists && pdfFile.canRead )
             val pdf = new Pdf(pdfFile)
