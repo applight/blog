1d95ff598a019fe547a988d9ded0032accfabe62
Matt Vaughan <admin@thenshow.me>
2022-02-26 10:31:27 -0500

    clean up repo gitignore

--- BlogFile.scala
+++ BlogFile.scala
@@ -3,6 +3,7 @@ package me.thenshow
 import java.io.File
 import scala.io.Source
 import sys.process._
+import scala.util.Properties
 
 abstract class BlogFile {
     def render(): String
@@ -10,28 +11,26 @@ abstract class BlogFile {
 
 case class Pdf(f:File) extends BlogFile {
     def render(): String = {
+        
         "<div style=\"text-align:center\"><h4>Pdf viewer testing</h4><iframe src=\"" +
         "https://docs.google.com/viewer?url=" + 
-        f.getPath() + 
+        Properties.envOrElse("PHPHOST", "dev.thenshow.me/blog") + "/" + f.getPath() + 
         "&embedded=true\" frameborder=\"0\" height=\"500px\" width=\"100%\"></iframe></div>"
     }
 }
 
 case class Tex(f:File) extends  BlogFile {
     def render(): String = {
-        if ( ("pdflatex -output-directory=./generatedPdfs -output-format=pdf " + f.getPath() !) == 0 )
-        {
-            val fileName = "./generatedPdfs" + f.getName().split(".tex").head + ".pdf"
-            println(fileName)
- 
-            val pdfFile = new File("./"+fileName)
-            assert( mv_ret == 0 && pdfFile.exists && pdfFile.canRead )
-            val pdf = new Pdf(pdfFile)
-            pdf.render()
-        } else {
-            s"<p>There was an error opening TeX file ${ f.getPath() }</p>"
-        }
+        val output = "pdflatex -output-directory=./generatedPdfs -output-format=pdf " + f.getAbsolutePath() !!
         
+        val fileName = "./generatedPdfs/" + f.getName().split(".tex").head + ".pdf"
+        println(fileName)
+
+        val pdfFile = new File(fileName)
+        assert( pdfFile.exists && pdfFile.canRead )
+        val pdf = new Pdf(pdfFile)
+        pdf.render()
+        //s"<p>There was an error opening TeX file ${ f.getPath() }</p>" 
     }
 }
 
